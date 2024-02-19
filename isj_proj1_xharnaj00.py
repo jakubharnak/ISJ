0eeae80b7d7cafa6db795ae1099082001650b510
jakubharnak <harnakjakub@gmail.com>
2024-02-19 11:20:11 +0100

    Create isj_proj1_xharnaj00.py

---
+++ isj_proj1_xharnaj00.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import re
+
+# ukol za 3 body
+def camel_to_snake_case(name):
+    """Transfer camelCaseNames to snake_case_names.
+
+    >>> camel_to_snake_case('camelCaseNameAllowed')
+    'camel_case_name_allowed'
+    >>> camel_to_snake_case('longVATNumber')
+    'long_vat_number'
+    """
+
+    inbetween = re.compile(r''' (?<!([A-Z]{1}))([a-z])(?=[A-Z]) ''', re.VERBOSE)
+    return inbetween.sub(r'_', name).lower()
+
+
+# ukol za 2 body
+def not_both_titles(names_string):
+    """Returns a list of names not preceded by [Pp]rof./[Dd]oc. and 
+    followed by ', Ph.D.'
+
+    >>> not_both_titles('doc. Josef Tyl, Rudolf Srp, Ph.D., Pavel Vlk, doc. RNDr. Petr Berka, Ph.D., Jan Hora')
+    ['doc. Josef Tyl', 'Rudolf Srp, Ph.D.', 'Pavel Vlk', 'Jan Hora']
+    """
+
+    pat = re.compile(r'''
+(?:
+  (?<=([Pp]rof\.|([Dd]oc\.)) ) (?=.* , Ph\.D\. )
+|
+  .*
+)
+''', re.X)
+
+
+
+if __name__ == "__main__":
+ import doctest
+ doctest.testmod()
