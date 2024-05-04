072d2b80989b43c5d7feae65c9ebcc217e8d69d0
jakubharnak <harnakjakub@gmail.com>
2024-05-04 18:16:10 +0200

    fix proj 8

--- projects/isj_proj8_xharnaj00.py
+++ projects/isj_proj8_xharnaj00.py
@@ -3,10 +3,11 @@ import asyncio
 import aiohttp
 
 async def fetch(session, url):
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0"}
     try:
-        async with session.get(url, headers=headers) as response:
+        async with session.get(url) as response:
             return response.status, url
+    except aiohttp.ClientConnectorError as e:
+        return "aiohttp." + e.__class__.__name__, url
     except Exception as e:
         return type(e).__name__, url
 
@@ -19,11 +20,13 @@ async def get_urls(urls):
     return responses
 
 if __name__ == '__main__':
+    
     urls = ['https://www.fit.vutbr.cz', 'https://www.szn.cz', 'https://office.com']
-
+    
     # for MS Windows
+    
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
+    
     res = asyncio.run(get_urls(urls))
-
+    
     print(res)
