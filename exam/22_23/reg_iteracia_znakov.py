eb50de96b45406b878910f3737c8924f37e48d65
jakubharnak <harnakjakub@gmail.com>
2024-05-06 00:27:02 +0200

    update

--- exam/22_23/reg_iteracia_znakov.py
+++ exam/22_23/reg_iteracia_znakov.py
@@ -1,9 +1,7 @@
 import re
 text = 'abbacccdee'
 
-result = [m[0] for m in re.finditer(
-                                    r'(.)\1*' # group of the same characters 
-                                    , text)]
+result = [m[0] for m in re.finditer(r'(a+|b+|c+|d+)', text)] # or r'(.)\1*'
 print(result)
 
 # Tento regulární výraz (.)\1* funguje tak, že (.) odpovídá jakémukoli znaku a \1* odpovídá nula nebo více opakování stejného znaku. 
--- exam/22_23/sort_pos_numbers.py
+++ exam/22_23/sort_pos_numbers.py
@@ -1,4 +1,7 @@
 print(sorted([8, 3, 2, 42, 5], key = lambda x: 0 if x == -42 or x == 42 else x))
+print(sorted([8, 3, 2, 42, 5], key = lambda x: 0 if x == 42 or x%2 else x))
+print(sorted([8, 3, 2, 42, 5], key = lambda x: 0 if x == 42 or x%3 else x))
+
 
 # sorted() je funkce, která seřadí daný seznam1.
 # Parametr key určuje, podle jakého klíče se mají prvky seřadit.
